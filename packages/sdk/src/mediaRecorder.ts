f1ef0a017dcfb5d26a640ec23f5644de01101486
akakream <akeremtuberlin@gmail>
2022-06-26 20:38:14 +0200

    websocket created. problematic with screen sharing. Works fine with camera

--- packages/sdk/src/mediaRecorder.ts
+++ packages/sdk/src/mediaRecorder.ts
@@ -1,16 +1,21 @@
+import { io } from "socket.io-client";
+
 const recordedChunks = [];
+let socket = null;
 
 export function setupMediaRecorder(stream: MediaStream) {
   const timeslice = 5000;
   const mediaRecorder = new MediaRecorder(stream);
   mediaRecorder.ondataavailable = handleDataAvailable;
   mediaRecorder.start(timeslice);
+  socket = io('http://localhost:3104', {transports: ['websocket'], upgrade: false});
 }
 
 function handleDataAvailable(event) {
   console.log("data-available");
   if (event.data.size > 0) {
     recordedChunks.push(event.data);
+    socket.emit('data', event.data);
     pipeToFfmpeg(recordedChunks);
   } else {
     console.log("event.data.size is <= 0.");
@@ -21,8 +26,19 @@ function pipeToFfmpeg(recordedChunks) {
   console.log(recordedChunks);
 
   let blob = new Blob(recordedChunks, {
-    type: "video/webm;codecs=vp8"
+    type: "video/webm"
   });
-  let video = URL.createObjectURL(blob);
-  console.log(video);
+  let video_url = URL.createObjectURL(blob);
+  console.log(video_url);
+  // downloadVideo(video_url);
+  window.URL.revokeObjectURL(video_url);
+}
+
+function downloadVideo(video_url) {
+  var a = document.createElement('a');
+  document.body.appendChild(a);
+  //a.style = 'display: none';
+  a.href = video_url;
+  a.download = 'test.webm';
+  a.click();
 }
